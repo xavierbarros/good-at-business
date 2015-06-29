86b5414e513335ff6785f5f6c0a42fcd1c2305e1
Marcelo Barros <xavierbarros@gmail.com>
2015-06-29 01:13:16 -0300

    The holy project is on the git by now.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.cta').click(function(){
+    $('.overlay').addClass('is-open');
+    return false;
+  });
+
+  $('.close-btn').click(function(){
+    $('.overlay').removeClass('is-open');
+  });
+});
